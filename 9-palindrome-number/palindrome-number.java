c6f579ef1055a66b760b806963cd7f3fe62f0003
SHASHWAT KUMAR <152202149+Shashwatk806@users.noreply.github.com>
2025-04-21 08:51:10 +0530

    Time: 8 ms (9.52%) | Memory: 43.7 MB (97.97%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s =Integer.toString(x);
+       if(isPal(s) && Integer.parseInt(s) == x){
+        return true;
+       }
+        return false;
+    }
+    public static boolean isPal(String s){
+         int i = 0; int j = s.length()-1;
+        while(i<=j){
+            if(s.charAt(i) !=s.charAt(j)){
+                return false;
+            }
+            i++;j--;
+        }
+        return true;
+    }
+}
