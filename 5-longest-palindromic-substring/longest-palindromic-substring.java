d5963b62f9d2ad41b4c12b714ecbe7ab4ec0e09c
SHASHWAT KUMAR <152202149+Shashwatk806@users.noreply.github.com>
2025-07-22 11:00:13 +0530

    Time: 246 ms (22.07%) | Memory: 55.8 MB (5.00%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,18 +1,30 @@
 class Solution {
+     int[][] dp;
     public boolean isPal(String s, int i, int j){
+       
         if(i>=j){
             return true;
         }
-        if(s.charAt(i) == s.charAt(j)){
-           return isPal(s,i+1,j-1);
+        if(dp[i][j] != -1){
+            return dp[i][j] == 1;
         }
+        if(s.charAt(i) == s.charAt(j)){
+        dp[i][j] = isPal(s,i+1,j-1) ? 1 : 0;
+        return dp[i][j] == 1;
+        }
+        dp[i][j] = 0;
         return false;
     }
     public String longestPalindrome(String s) {
+        int n = s.length();
+         dp = new int[n][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
        int maxLength = Integer.MIN_VALUE;
        int idx = 0;
-       for(int i= 0;i<s.length();i++){
-        for(int j=i;j<s.length();j++){
+       for(int i= 0;i<n;i++){
+        for(int j=i;j<n;j++){
             if(isPal(s,i,j) == true){
                 if(j-i+1 > maxLength){
                     maxLength = j-i+1;
