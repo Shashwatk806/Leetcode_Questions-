ff1569ed3a6f8b9a4271e5f7dd6f96fad4a295d5
SHASHWAT KUMAR <152202149+Shashwatk806@users.noreply.github.com>
2025-09-16 14:42:40 +0530

    Time: 5 ms (95.81%) | Memory: 43.1 MB (43.40%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -2,28 +2,28 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         return solve(s1,s2);
     }
-    public boolean solve(String s1,String s2){
-        int[] s1count = new int[26];
-        int[] s2count = new int[26];
+    public boolean solve(String s1, String s2){
         int n = s1.length();
+        int m = s2.length();
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
         for(int i=0;i<n;i++){
-            s1count[s1.charAt(i)-'a']++;
+            freq1[s1.charAt(i)-'a']++;
         }
-        int i=0;
-        int j = 0;
-        while(j<s2.length()){
-            s2count[s2.charAt(j)-'a']++;
+        int i=0; int j = 0;
+        while(j<m){
+            freq2[s2.charAt(j)-'a']++;
             if(j-i+1>n){
-                s2count[s2.charAt(i)-'a']--;
+                freq2[s2.charAt(i)-'a']--;
                 i++;
             }
-            if(matches(s1count,s2count)) return true;
+            if(matches(freq1,freq2)) return true;
             j++;
         }
         return false;
     }
-     private boolean matches(int[] a,int[] b) {
-        for (int i=0;i<26;i++) {
+    private boolean matches(int[] a, int[] b){
+        for(int i=0;i<26;i++){
             if(a[i] != b[i]) return false;
         }
         return true;
