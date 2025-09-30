c6e1f60c5e25d97b1b4f2ff7e4ddbda6c6158da9
SHASHWAT KUMAR <152202149+Shashwatk806@users.noreply.github.com>
2025-09-30 15:33:49 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (28.65%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,31 +1,20 @@
 class Solution {
-    int[][] dp;
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-         if (obstacleGrid[0][0] == 1 || obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1] == 1) {
-            return 0;
-        }
-        dp = new int[obstacleGrid.length][obstacleGrid[0].length];
-        for(int[] r : dp){
+        int m = obstacleGrid.length; int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for(int[] r: dp){
             Arrays.fill(r,-1);
         }
-         return solve(obstacleGrid,0,0);
+        return solve(obstacleGrid,0,0,dp);
     }
-    public int solve(int[][] arr, int m, int n){
-        if(m == arr.length-1 && n == arr[0].length-1){
-            return 1;
-        }
-        if(m>=arr.length || n>= arr[0].length){
-            return 0;
-        }
-        if(arr[m][n] == 1){
-            return 0;
-        }
+    private int solve(int[][] arr, int m, int n, int[][] dp){
+        if(m>=arr.length || n>=arr[0].length || arr[m][n] == 1) return 0;
         if(dp[m][n] != -1){
             return dp[m][n];
         }
-        int right = solve(arr,m,n+1);
-        int bottom =solve(arr,m+1,n);
-        dp[m][n] = right + bottom;
-        return dp[m][n];
+        if(m==arr.length-1 && n==arr[0].length-1) return 1;
+        int down = solve(arr,m+1,n,dp);
+        int right = solve(arr,m,n+1,dp);
+        return dp[m][n] = down+right;
     }
 }
