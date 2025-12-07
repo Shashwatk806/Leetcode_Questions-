25c66ea570859c1d9b5364afc97329b38daebb8e
SHASHWAT KUMAR <152202149+Shashwatk806@users.noreply.github.com>
2025-12-07 18:14:47 +0530

    Time: 7 ms (69.28%) | Memory: 46.1 MB (54.12%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,21 +1,24 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        return lcs(word1,word2);
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int[] r : dp){
+            Arrays.fill(r, -1);
+        }
+        int a = solve(word1,word2,0,0,dp);
+        int length = (m-a)+(n-a);
+        return length;
     }
-      public static int lcs(String s1, String s2) {
-			int[][] dp = new int[s1.length()+1][s2.length()+1];
-		for(int i = 1;i<dp.length;i++) {
-			for(int j = 1;j<dp[0].length;j++) {
-				if(s1.charAt(i-1) == s2.charAt(j-1)) {
-					dp[i][j] = 1+dp[i-1][j-1];
-				}else {
-					int a = dp[i-1][j];
-					int b = dp[i][j-1];
-					dp[i][j] = Math.max(a, b);
-				}
-			}
-		}
-		int length = dp[dp.length-1][dp[0].length-1];
-        return (s1.length()-length)+(s2.length()-length);
-	}
+    public int solve(String s1, String s2, int i , int j, int[][] dp){
+        if(i==s1.length() || j==s2.length()){
+            return 0;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s1.charAt(i)==s2.charAt(j)){
+            return dp[i][j] = 1+solve(s1,s2,i+1,j+1,dp);
+        }else{
+            return dp[i][j] = Math.max(solve(s1,s2,i+1,j,dp), solve(s1,s2,i,j+1,dp));
+        }
+    }
 }
