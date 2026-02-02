7f089c34e0795a2363691d5fafc63a7b46098e2b
SHASHWAT KUMAR <152202149+Shashwatk806@users.noreply.github.com>
2026-02-02 21:02:47 +0530

    Time: 11 ms (65.93%) | Memory: 47.4 MB (50.14%) - LeetSync

--- 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.java
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean hasGroupsSizeX(int[] deck) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int num : deck){
-            map.put(num, map.getOrDefault(num,0)+1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int n : deck){
+            map.put(n, map.getOrDefault(n,0)+1);
         }
         int gcd = 0;
-        for(int freq : map.values()){
-            gcd = gcd==0?freq:gcd(gcd,freq);
+        for(int num : map.values()){
+            gcd = gcd==0?num:gcd(gcd,num);
         }
-        return gcd >=2;
+        return gcd>=2;
     }
     private int gcd(int a, int b){
         if(b==0) return a;
