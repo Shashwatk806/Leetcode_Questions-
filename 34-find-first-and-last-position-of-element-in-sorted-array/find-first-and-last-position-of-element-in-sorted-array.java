26e7b642b19ecb028d3899e28c643c82e94d15c9
SHASHWAT KUMAR <152202149+Shashwatk806@users.noreply.github.com>
2026-02-27 12:43:48 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (28.72%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,40 +1,42 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = new int[2];
-        ans[0] = first(nums,target);
-        ans[1] = last(nums,target);
-        return ans;
+        int[] res = new int[2];
+        res[0] = first(nums,target);
+        res[1] = last(nums,target);
+        return res;
     }
-    private int first(int[] nums, int target){
+    private int first(int[] nums,int target){
         int l = 0;
         int r = nums.length-1;
-        int idx = -1;
+         int idx = -1;
         while(l<=r){
             int mid = l+(r-l)/2;
+           
             if(nums[mid]==target){
                 idx = mid;
-                r=mid-1;
-            }else if(nums[mid]<target){
-                l=mid+1;
+                r = mid-1;
+            }else if(nums[mid]>target){
+                r = mid-1;
             }else{
-                r=mid-1;
+                l=mid+1;
             }
         }
         return idx;
     }
-      private int last(int[] nums, int target){
+    private int last(int[] nums,int target){
         int l = 0;
         int r = nums.length-1;
-        int idx = -1;
+         int idx = -1;
         while(l<=r){
             int mid = l+(r-l)/2;
+           
             if(nums[mid]==target){
                 idx = mid;
-                l=mid+1;
-            }else if(nums[mid]<target){
-                l=mid+1;
+                l= mid+1;
+            }else if(nums[mid]>target){
+                r = mid-1;
             }else{
-                r=mid-1;
+                l=mid+1;
             }
         }
         return idx;
