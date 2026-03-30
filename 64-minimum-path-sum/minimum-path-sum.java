7aa1668558c6b6b5cabb176535123792947ebea0
SHASHWAT KUMAR <152202149+Shashwatk806@users.noreply.github.com>
2026-03-30 20:17:00 +0530

    Time: 1 ms (99.77%) | Memory: 49.9 MB (92.02%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -5,21 +5,22 @@ class Solution {
             Arrays.fill(r,-1);
         }
         return solve(grid,0,0,dp);
+        
     }
-    private int solve(int[][] arr, int m, int n,int[][] dp){
-        if(m==arr.length-1 && n==arr[0].length-1){
-            dp[m][n] = arr[m][n];
-            return dp[m][n];
+    private int solve(int[][] arr, int i, int j, int[][] dp){
+        if(i==arr.length-1 && j==arr[0].length-1){
+            dp[i][j] = arr[i][j];
+            return dp[i][j];
         }
-         if(m>=arr.length || n>= arr[0].length){
+        if(i>=arr.length || j>=arr[0].length){
             return Integer.MAX_VALUE;
         }
-         if(dp[m][n] != -1){
-            return dp[m][n];
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
-        int down =solve(arr,m+1,n,dp);
-        int right = solve(arr,m,n+1,dp);
-        dp[m][n] = Math.min(down,right) + arr[m][n];
-        return dp[m][n];
+        int down = solve(arr,i+1,j,dp);
+        int right = solve(arr,i,j+1,dp);
+         dp[i][j] = arr[i][j]+Math.min(down,right);
+         return dp[i][j];
     }
 }
